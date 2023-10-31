cc0cd484a9169044e2c9f9831c9b0cce22ced1ac
GuiMedeirox <omaewa9111@gmail.com>
2023-10-31 16:44:58 -0300

    feat: cliente create + read

--- libs/clientesLib.h
+++ libs/clientesLib.h
@@ -1,1 +1,12 @@
-void menuClientes(); 
+typedef struct Cliente Cliente; 
+struct Cliente{ 
+  char nome[100]; 
+  char cpf[12]; 
+  char telefone[11]; 
+};
+void menuClientes();
+void criarCliente();
+void exibirClientes();
+void buscarCliente();
+void editarCliente();
+void deletarCliente();
