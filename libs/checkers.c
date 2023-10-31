36c868c35e001172fe4876b28ed81d0cb8ac5290
GuiMedeirox <omaewa9111@gmail.com>
2023-10-31 16:46:20 -0300

    fix checkers

--- libs/checkers.c
+++ libs/checkers.c
@@ -4,31 +4,46 @@
 #include <stdio.h> 
 #include <ctype.h>
 #include <string.h> 
+
 // flavius, obrigado por passar a logica de como alguns checkers devem ser feitos
 
-int checkCPF(char* cpf){
-  if (strlen(cpf) != 11) {
-    printf("CPF inválido.\n");
+int checkCPF(char cpf[]) {
+    int i, j, digito1 = 0, digito2 = 0;
+
+    if (strlen(cpf) != 11){
+      return 0;
+    }
+        
+    else if ((strcmp(cpf, "00000000000") == 0) || (strcmp(cpf, "11111111111") == 0) || (strcmp(cpf, "22222222222") == 0) || (strcmp(cpf, "33333333333") == 0) || (strcmp(cpf, "44444444444") == 0) || (strcmp(cpf, "55555555555") == 0) || (strcmp(cpf, "66666666666") == 0) || (strcmp(cpf, "77777777777") == 0) || (strcmp(cpf, "88888888888") == 0) || (strcmp(cpf, "99999999999") == 0)){
+        return 0;
+    }
+    else {
+        for (i = 0, j = 10; i < strlen(cpf) - 2; i++, j--)
+            digito1 += (cpf[i] - '0') * j;
+        digito1 %= 11;
+        if (digito1 < 2)
+            digito1 = 0;
+        else
+            digito1 = 11 - digito1;
+        if ((cpf[9] - '0') != digito1)
+            return 0;
+        else {
+            for (i = 0, j = 11; i < strlen(cpf) - 1; i++, j--)
+                digito2 += (cpf[i] - '0') * j;
+            digito2 %= 11;
+            if (digito2 < 2)
+                digito2 = 0;
+            else
+                digito2 = 11 - digito2;
+            if ((cpf[10] - '0') != digito2)
+                return 0;
+        }
+    }
     return 1;
-  }
-
-  int digito1 = 0, digito2 = 0;
-  for (int i = 0; i < 9; i++) {
-    digito1 += (cpf[i] - '0') * (10 - i);
-    digito2 += (cpf[i] - '0') * (11 - i);
-  }
-  digito1 = (digito1 % 11) < 2 ? 0 : 11 - digito1;
-  digito2 = (digito2 % 11) < 2 ? 0 : 11 - digito2;
-
-  if (cpf[9] != digito1 || cpf[10] != digito2) {
-    return 1;
-  }
-
-  return 0;
 }
 
 
-int checkDigito(char* array){
+int checkDigito(char array[]){
   int flag; 
   for(int i = 0; array[i]!='\0'; i++){
     if(array[i] >= '0' || array[i] <='9'){ 
@@ -40,7 +55,7 @@ int checkDigito(char* array){
   return flag;
 }
 
-int checkTelefone(char* array){
+int checkTelefone(char array[]){
   if(strlen(array)==11){
     int flag = checkDigito(array);
     if(flag == 1){
@@ -55,18 +70,24 @@ int checkLetra(char c) {
         return 1;
     } else if (c >= 'a' && c <= 'z') {
         return 1;
-    } else {
+    } else if(c == ' '){
+        return 1; 
+    } 
+    else {
         return 0;
     }
 }
 
-int checkNome(char* nome) {
-    for (int i=0; nome[i]!='\0'; i++) {
-        if (!checkLetra(nome[i])) {
-            return 0;
-        }
-    }
-    return 1;
+int checkNome(char nome[]) {
+    int flag; 
+    for (int i=0; i < strlen(nome) ; i++) {
+      if(checkLetra(nome[i]) == 1 ){
+        flag=1; 
+      }else{
+        flag=0;
+      }
+  }
+    return flag;
 }
 
 void clear(){
