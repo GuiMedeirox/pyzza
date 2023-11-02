cac88f34728533952c51da91be6dc744218d4a4a
GuiMedeirox <omaewa9111@gmail.com>
2023-11-02 14:07:01 -0300

    funcionarios READ

--- libs/funcionarioLib.c
+++ libs/funcionarioLib.c
@@ -1,13 +1,82 @@
 #include <stdio.h> 
+#include <stdlib.h>
+#include <string.h> 
+#include <ctype.h> 
 #include "funcionarioLib.h"
+#include "checkers.h"
 
-struct Funcionario{
-  char* nome; 
-  char* cpf; 
-  char* telefone; 
-  char* funcao; 
-};
+void criarFuncionario(){
+  clear();
+  FILE* file = fopen("funcionarios.dat", "ab");
+  Funcionario* f = (Funcionario*) malloc(sizeof(Funcionario));
+  printf("--- Cadastro de Funcionários ---");
+  
+  do{
+    printf("\nDigita o nome do funcionário: ");
+    scanf(" %100[^\n]", f->nome);
 
+    printf("Digita o CPF do funcionário: ");
+    scanf(" %11[0-9]", f->cpf);
+    getchar();
+    
+    printf("Digita o telefone do funcionário: ");
+    scanf(" %11[0-9]", f->telefone); 
+    getchar();
+
+    printf("Digita a função do funcionário: ");
+    scanf(" %50[^\n]", f->funcao);
+    getchar();
+  } while(checkTelefone(f->telefone) == 0 && checkNome(f->funcao) == 0 && checkCPF(f->cpf) == 0 && checkNome(f->nome) == 0 );
+  
+  printf("\nFuncionario cadastrado com sucesso!\n");
+  printf("\nO nome escolhido foi: %s", f->nome);
+  printf("\nTelefone: %s", f->telefone);
+  printf("\nO CPF eh %s e o cargo: %s ", f->cpf, f->funcao); 
+  fwrite(f, sizeof(Funcionario), 1, file);
+  free(f);
+  fclose(file); 
+  }
+
+
+
+void lerFuncionarios(){
+  
+  FILE *file = fopen("funcionarios.dat", "rb");
+  Funcionario *f = (Funcionario*) malloc(sizeof(Funcionario));  
+  if (file == NULL){
+    printf("Erro ao abrir o arquivo de clientes!");
+    exit(1);
+  }
+
+  while( fread(f, sizeof(Funcionario), 1, file) ){
+    printf("%s | %s | %s | %s", f->nome, f->cpf, f->telefone, f->funcao);
+  }
+  fclose(file);
+}
+void buscarFuncionario(){
+  clear();
+  Funcionario *f = (Funcionario*) malloc(sizeof(Funcionario));
+  FILE* file = fopen("funcionarios.dat", "rb");
+  char cpf[12];
+
+  if(file == NULL){
+    printf("Erro ao abrir o arquivo!"); 
+    exit(1);
+  }
+
+  printf("Digita o CPF do funcionário a ser buscado: ");
+  scanf(" %12[^\n]", cpf);
+  
+  while(!feof(file)){
+    fread(f, sizeof(Funcionario), 1, file);
+    if( strcmp(f->cpf, cpf) == 0 ) {
+      printf("O funcionario eh: %s", f->nome);
+      fclose(file);
+      break;
+    }
+  }
+  free(f);
+}
 void menuFuncionario(){
   int opcao; 
 
@@ -21,17 +90,14 @@ void menuFuncionario(){
 
   switch (opcao){
     case 1: 
-    printf("Digita o nome do funcionário: \n"); 
-    printf("Digita o CPF do funcionário: \n"); 
-    printf("Digita o telefone do funcionário: \n"); 
-    printf("Digita a função do funcionário: \n");
-    break;
+      criarFuncionario();
+      break;
     case 2: 
-    printf("Digita o CPF do funcionário: \n");
-    break;
+      buscarFuncionario();
+      break;
     case 3: 
-    //showfuncionarios(); 
-    break;
+      lerFuncionarios();
+      break;
     case 4: 
     printf("Digita o CPF do funcionário a ser editado: \n");
     break;
--- libs/funcionarioLib.h
+++ libs/funcionarioLib.h
@@ -1,1 +1,14 @@
+typedef struct Funcionario Funcionario;
+struct Funcionario{
+  char nome[100]; 
+  char cpf[12]; 
+  char telefone[12]; 
+  char funcao[50]; 
+};
+
 void menuFuncionario();
+void criarFuncionario();
+void lerFuncionarios();
+void buscarFuncionario();
+void editarFuncionario();
+void deletarFuncionario();
