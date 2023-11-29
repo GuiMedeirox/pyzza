e0cd9bd4b8c0bdc597f98ea112c195db9abade41
GuiMedeirox <omaewa9111@gmail.com>
2023-11-29 18:54:49 -0300

    all.h

---
+++ libs/all.h
@@ -0,0 +1,13 @@
+#ifndef UTIL_H  // Evita problemas de inclusão múltipla
+#define UTIL_H
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <ctype.h> 
+#include "clientesLib.h"
+#include "funcionarioLib.h"
+#include "pedidosLib.h"
+#include "pizzasLib.h"
+#include "checkers.h"
+typedef struct Pizza Pizza;
+#endif
