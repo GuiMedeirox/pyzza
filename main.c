afa77eab11d5b73cdbc0e9f5d4c26ce18d024a3c
GuiMedeirox <omaewa9111@gmail.com>
2023-12-01 19:59:57 -0300

    feat: main

--- main.c
+++ main.c
@@ -37,7 +37,7 @@ int main() {
             case 5: 
                 clear();
                 printf("Projeto feito e utilizado na disciplina de programação, o intuito é simples: suprir todas (ou quase) as funcionalidades que um sistema de uma pizzaria pode ter.\n"); 
-                printf("Um agradecimento especial a: Eduardo dc:(eduardoferro), Tesch dc:(ptesch) ");
+                printf("Um agradecimento especial a:\n Eduardo dc:(eduardoferro);\n Tesch dc:(ptesch) \n Gabriel Canuto ");
                 printf("dev: Guilherme\n");
                 printf("GitHub: @GuiMedeirox\n"); 
             case 0:
