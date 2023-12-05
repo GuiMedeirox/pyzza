d331add2c423cde2e1fef6a2a03019a5da771e4d
GuiMedeirox <omaewa9111@gmail.com>
2023-12-05 14:03:45 -0300

    feat: retornaCliente()

--- libs/clientesLib.h
+++ libs/clientesLib.h
@@ -1,11 +1,11 @@
 
-typedef struct Cliente Cliente; 
-struct Cliente{ 
+typedef struct Cliente{ 
   char nome[100]; 
   char cpf[12]; 
   char telefone[12]; 
   int status; 
-};
+}Cliente;
+
 void menuClientes();
 void criarCliente();
 void exibirClientes();
@@ -14,3 +14,4 @@ void editarCliente();
 void deletarCliente();
 int verificaCPFDuplicado( char* cpf);
 char *encontraCliente(char* cpf);
+Cliente retornaCliente(const char* cpf);
